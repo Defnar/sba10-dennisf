50552b297d8aebf24f73bd72853334d9ee8dea5e
Dennis <dennis-farris@hotmail.com>
2025-08-09 12:54:42 -0400

    implemented a debounced, dynamic search bar to resultlist page to search through results

--- src/Pages/ResultList.tsx
+++ src/Pages/ResultList.tsx
@@ -1,11 +1,14 @@
 import { Link, useParams } from "react-router-dom";
 import type { Recipe, ResultListProp } from "../utils/types";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import generateURL from "../utils/generateURL";
 import useFetch from "../hooks/useFetch";
+import SearchBar from "../components/SearchBar";
 
 export default function ResultList({ listType }: ResultListProp) {
   const { region, category, letter } = useParams();
+
+  const [searchResults, setSearchResults] = useState<string>("");
 
   const endOfUrl = useMemo(() => {
     switch (listType) {
@@ -13,8 +16,8 @@ export default function ResultList({ listType }: ResultListProp) {
         return `filter.php?c=${category}`;
       case "region":
         return `filter.php?a=${region}`;
-        case "letter":
-          return `search.php?f=${letter}`
+      case "letter":
+        return `search.php?f=${letter}`;
     }
   }, [category, region, listType, letter]);
 
@@ -22,20 +25,41 @@ export default function ResultList({ listType }: ResultListProp) {
   const url = generateURL(endOfUrl);
   const { loading, data, error } = useFetch<Recipe>(url);
 
+  const filteredList = useMemo(() => {
+    if (data)
+      return data.meals.filter((recipe) => {
+        return recipe.strMeal
+          .toLowerCase()
+          .includes(searchResults.toLowerCase().trim());
+      });
+  }, [data, searchResults]);
+
   //create a list of categories or regions to render
   const listSetup = useMemo(() => {
     if (loading) return <p>Loading section...</p>;
-    if (data) {
-      return data.meals.map((meal) => (
+    if (filteredList) {
+      return filteredList.map((meal) => (
         <Link key={meal.idMeal} to={`/recipe/${meal.idMeal}`}>
           <h3>{meal.strMeal}</h3>
-          <img src={meal.strMealThumb} alt={`image of ${meal.strMeal}`} />
+          {meal.strMealThumb && (
+            <img src={meal.strMealThumb} alt={`image of ${meal.strMeal}`} />
+          )}
         </Link>
       ));
     }
 
     if (error) return <p>{error}</p>;
-  }, [data, error, loading]);
+  }, [filteredList, error, loading]);
 
-  return <div>{listSetup}</div>;
+  return (
+    <div>
+      <SearchBar
+        currentSearch={setSearchResults}
+        debounceTimer={250}
+        isDynamic={true}
+        displaySearchButton={false}
+      />
+      {listSetup}
+    </div>
+  );
 }
